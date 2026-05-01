749069d7a6f5a4b02f48fed50be612badb5e43bc
Gustav F. Lasko <gustav.lasko@gmail.com>
2026-05-01 16:01:59 +0000

    feat: add error endpoint
    We add a get endpoint to the API that throws an exception. This allows
    use test our error metrics and logs.

--- backend/src/main/java/com/interview/resource/WelcomeResource.java
+++ backend/src/main/java/com/interview/resource/WelcomeResource.java
@@ -1,5 +1,6 @@
 package com.interview.resource;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +11,10 @@ public class WelcomeResource {
     public String index() {
 
         return "Welcome to the interview project!";
+    }
+
+    @GetMapping("/api/error")
+    public String error() {
+        throw new RuntimeException("This is a test exception!");
     }
 }
