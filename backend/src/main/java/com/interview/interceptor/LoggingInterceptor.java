034b6bb2aae37da8ab4e30a6257ad4a29e876d6a
Gustav F. Lasko <gustav.lasko@gmail.com>
2026-05-01 12:52:17 +0000

    chore: add logging to the backend
    We add logging to the backend.
    We add `INFO` level for every request by adding a HandlerInterceptor to
    intercept every request befor it reaches the controller. We also add
    exception logging that logs any unhandled exceptions from any
    controller, and will log the full stack trace.

---
+++ backend/src/main/java/com/interview/interceptor/LoggingInterceptor.java
@@ -0,0 +1,20 @@
+package com.interview.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    private static final Logger log = LoggerFactory.getLogger(LoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        log.info("{} {}", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+}
